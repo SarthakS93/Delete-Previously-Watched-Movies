879fef5349543d0569ffa07fe972cf845f9a7981
sarthak <sarthaks93@gmail.com>
2016-11-29 00:38:24 +0530

    added extensions map

--- deleteUnusedFiles.py
+++ deleteUnusedFiles.py
@@ -3,21 +3,37 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 now = datetime.now()
-ref_time = now - relativedelta(months = 6)
+ref_time = now - relativedelta(seconds = 10)
 print(now)
 print(ref_time)
 
-def func():
-    files = os.listdir()
-    for f in files:
-        stats = os.stat(f)
-        access_time = datetime.fromtimestamp(stats.st_atime)
-        print(f, access_time)
-        if access_time < ref_time:
-            print(f, '---')
-
-func()
+ext = { 'mp4':,'Media' 'mkv':'Media', 'wmv':'Media', 'mpeg':'Media', 'flv':'Media'
+        'avi':'Media', 'm4v':'Media', 'txt':'Text', 'srt':'Text', 'nfo':'Text',
+        'url':'Shortcut','htm':'Shortcut', 'html':'Shortcut', 'website':'Shortcut',
+        'jpg':'Media', 'png':'Media',
+      }
 
 
+
+def getAccessTime(stat):
+    return datetime.fromtimestamp(stat.st_atime)
+
+
+def deleteFiles():
+    tree = os.walk(os.getcwd())
+
+    for dirName, subDirs, files in tree:
+        print('Inside ', dirName)
+        totalNumberOfFiles = len(files)
+        filesVisited = 0
+        for f in files:
+            filesVisited += 1
+            filepath = os.path.join(dirName, f)
+            access_time =  getAccessTime(os.stat(filepath))
+            if access_time < ref_time:
+                print('Deleting ', f)
+
+
+deleteFiles()
 #if __name__ == '__main__':
 
