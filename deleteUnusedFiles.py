f1cf65a9f7b610f22024e352d3e77db5c02b0d7e
sarthak <sarthaks93@gmail.com>
2016-11-29 01:19:54 +0530

    first draft ready

--- deleteUnusedFiles.py
+++ deleteUnusedFiles.py
@@ -5,9 +5,9 @@ from dateutil.relativedelta import relativedelta
 now = datetime.now()
 ref_time = now - relativedelta(seconds = 10)
 print(now)
-print(ref_time)
+#print(ref_time)
 
-ext = { 'mp4':,'Media' 'mkv':'Media', 'wmv':'Media', 'mpeg':'Media', 'flv':'Media'
+ext = { 'mp4':'Media', 'mkv':'Media', 'wmv':'Media', 'mpeg':'Media', 'flv':'Media',
         'avi':'Media', 'm4v':'Media', 'txt':'Text', 'srt':'Text', 'nfo':'Text',
         'url':'Shortcut','htm':'Shortcut', 'html':'Shortcut', 'website':'Shortcut',
         'jpg':'Media', 'png':'Media',
@@ -27,12 +27,21 @@ def deleteFiles():
         totalNumberOfFiles = len(files)
         filesVisited = 0
         for f in files:
-            filesVisited += 1
-            filepath = os.path.join(dirName, f)
-            access_time =  getAccessTime(os.stat(filepath))
-            if access_time < ref_time:
-                print('Deleting ', f)
-
+            if f.split('.')[-1] in ext:
+                filepath = os.path.join(dirName, f)
+                access_time =  getAccessTime(os.stat(filepath))
+                if access_time < ref_time:
+                    print('Deleting ', f)
+                    try:
+                        os.remove(filepath)
+                    except:
+                        print('Error while deleting FILE ', f)
+        if len(os.listdir(dirName)) == 0:
+            print('Deleting directory')
+            try:
+                os.rmdir(dirName)
+            except:
+                print('Error while deleting DIRECTORY ', dirname)
 
 deleteFiles()
 #if __name__ == '__main__':
